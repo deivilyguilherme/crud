f1f0998a2ed441d4477b44bfd03150ec5e6e0b8e
deivilyguilherme <deivilyguilherme2050@gmail.com>
2023-03-23 20:18:53 -0300

    meu primeiro crud

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
        <div id="botao"> <a class="btn btn-primary" href="/TESTE CRUD/create.php" role="button">Novo Convidado</a> </div>  
         <br>
         <section>
-        <table class="table table-striped">
+        <table class="ta">
             <thead>
                 <tr>
                     <th>Nome</th>
